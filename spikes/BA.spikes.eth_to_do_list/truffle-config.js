87e3fbf7d5ad2bec4a7a2faa2dc3984c2c8a1363
Avokaci <if19b123@technikum-wien.at>
2021-01-25 15:45:27 +0100

    Reorganisation des Repos und Trello Boards

---
+++ spikes/BA.spikes.eth_to_do_list/truffle-config.js
@@ -0,0 +1,18 @@
+module.experts = {
+  networks: {
+    development: {
+        host:"127.0.0.1",
+        port: 7545,
+        network_id:"*"
+    },
+
+    
+  },
+  solc: {
+    optimizer:{
+      enabled:true,
+      runs:200
+    }
+  }
+
+}
